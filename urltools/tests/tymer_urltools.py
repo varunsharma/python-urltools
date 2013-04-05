ad349d04dca0f3e3d7c0478821c8b4f55f282480
rbaier <roderick.baier@gmail.com>
2013-04-06 00:08:58 +0200

    tymer

--- urltools/tests/tymer_urltools.py
+++ urltools/tests/tymer_urltools.py
@@ -1,30 +1,30 @@
 # -*- coding: utf-8 -*-
 
-import tymer
+from tymer import t, run, skip
 
 from urltools.urltools import normalize, parse, extract, encode, split
 from urltools.urltools import _assemble
 
 
-setup_timeit = """
+setup_tymer = """
 from __main__ import normalize, parse, extract, encode, split
 from __main__ import _assemble
 """
 
 
-@tymer.skip
-def timeit_parse():
-    print "parse", tymer.t('parse("http://example.com")')
+#@skip
+def tymer_parse():
+    t('parse("http://example.com")')
 
 
-#@tymer.skip
-def timeit__assemble():
+#@skip
+def tymer__assemble():
     stmt = """
     parts = extract("http://www.example.com:8080/abc?x=1#rt")
     _assemble(parts)
     """
-    print "_assemble", tymer.t(stmt)
+    t(stmt)
 
 
 if __name__ == '__main__':
-    tymer.run()
+    run()

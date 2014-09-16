f8b8128784c64c2c4f1810c9f1098476aa6bb9d3
roderick <mail@roderick.de>
2014-09-16 12:04:43 +0200

    added classifiers; moved license

--- setup.py
+++ setup.py
@@ -12,5 +12,13 @@ setup(
     author_email='roderick.baier@gmail.com',
     license='MIT',
     url='https://github.com/rbaier/python-urltools',
-    packages=find_packages(exclude=['tests'])
+    packages=find_packages(exclude=['tests']),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities'
+    ]
 )
